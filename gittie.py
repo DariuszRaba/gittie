76c0c0a9e80ccbde02f6a9cd2ca8e86b99022947
ntiany <krolkas@gmail.com>
2019-12-08 10:27:06 +0100

    Correct spaces

--- gittie.py
+++ gittie.py
@@ -1,10 +1,10 @@
 def introduce():
     print("Hello, I'm Gittie!")
+    
 
-<<<<<<< HEAD
 def add(a,b):
     return a+b
-=======
+
+
 def joke():
     print("It's a beautiful day")
->>>>>>> 63d1f275e17b30f05e31d48bb151b903ff84d129
